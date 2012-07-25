c5facd1b052976d2b21a15404f2ad0030398007c
Bernardo Heynemann <heynemann@gmail.com>
2012-07-25 15:19:50 -0300

    App working

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from flask import Flask, render_template
 from fisl_keynotes.datasource import GlobaisTalks
 
@@ -6,7 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html', talks=GlobaisTalks().all_talks())
+    all_talks = [talk for talk in GlobaisTalks().all_talks() if talk.date >= datetime.now()]
+    return render_template('index.html', talks=all_talks)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
